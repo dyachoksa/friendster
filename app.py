0d635e91ae6667510e694426898619ddb6dbe6c1
Serhii Diachok <sergey@sdyachok.com.ua>
2021-11-03 20:30:37 +0200

    Added application name and version

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 # Friendster Application
-print("Welcome to the Friendster v1.0!")
+application_name = "Friendster"
+version = "1.0"
+
+print("Welcome to the {0}. Version v{1}!".format(application_name, version))
