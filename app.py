cf2de84065d3055fa120a73b61b4cd77cd7bb445
Serhii Diachok <sergey@sdyachok.com.ua>
2021-11-06 13:38:28 +0200

    Added main entry point

--- app.py
+++ app.py
@@ -2,4 +2,5 @@
 application_name = "Friendster"
 version = "1.0"
 
-print("Welcome to the {0}. Version v{1}!".format(application_name, version))
+if __name__ == "__main__":
+    print("Welcome to the {0}. Version v{1}!".format(application_name, version))
